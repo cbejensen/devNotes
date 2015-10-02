212074e59387e1dfca92d58e19a7b0266ba04fb6
Christian <cbejensen@gmail.com>
2015-10-01 19:05:05 -0600

    notes on arrays

---
+++ arrays.js
@@ -0,0 +1,13 @@
+//ARRAYS
+
+var arr = ["alligator", "buffalo", "crocodile"];
+
+//first item is 0, not 1
+var alli = arr[0];
+var buff = arr[1];
+var croc = arr[2];
+
+//this would be 3
+arr.length; 
+
+
